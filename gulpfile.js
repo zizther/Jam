7b564465d9670d5c40a8cb94d934932934006eaf
Nathan Reed <zizther@gmail.com>
2018-05-16 15:13:32 +0100

    Grid update, cleaing up gulp file, minor improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,68 @@
 /*
+ * Variables
+ */
+var paths = {
+    styles: {
+        scss: './scss/',
+        css: './css/'
+    }
+};
+
+
+/*
  * NPM Packages
  */
 var gulp = require('gulp'),
-    autoprefixer = require('autoprefixer'),
-    pump = require('pump');
-	postcss = require('gulp-postcss'),
-    sass = require('gulp-sass'),
-    environments = require('gulp-environments'),
-    gcmq = require('gulp-group-css-media-queries'),
-    cssnano = require('gulp-cssnano'),
-    notify = require('gulp-notify');
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    exec = require('child_process').exec,
+    pump = require('pump'),
+	autoprefixer = require('autoprefixer');
 
-/*
- * Variables
- */
-var development = environments.development,
-    production = environments.production;
+const $ = gulpLoadPlugins(); // Load all plugins which start with 'gulp-'. Reference using $.
 
-var paths = {
-    scss: './scss/',
-    css: './css/',
-};
-
-var autoprefixerBrowsers = ['last 2 versions', 'ie >= 9'],
-	sassPrecision = 10;
 
 /*
  * Tasks
  */
+// NPM Tasks
+gulp.task('exec-postcss', function (cb) {
+    exec('npm run postcss', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+})
 
- // Sass
- gulp.task('scss', function(cb){
-     var processors = [
-         autoprefixer({ browsers: autoprefixerBrowsers })
-     ];
+// Scss
+gulp.task('scss', function() {
+    gulp.src(paths.styles.scss + '**/*.scss')
+    	.pipe($.sass({
+	    	precision: 10,
+	    	sourceComments: false,
+	    	outputStyle: 'compressed'
+    	}).on('error', $.sass.logError))
+		.pipe(gulp.dest(paths.styles.css))
+        .pipe($.notify({ message: 'Scss task complete' }));
+});
 
-     pump(
-         [
-            gulp.src(paths.scss + '**/*.scss'),
-            sass({
-      	        precision: sassPrecision,
-      	        sourceComments: production() ? false : true,
-      	        outputStyle: 'nested'
-  	        }).on('error', sass.logError),
-          	postcss(processors),
-            production(gcmq()),
-            production(cssnano()),
-      		gulp.dest(paths.css),
-            notify({ message: 'Scss task complete' })
-         ],
-         cb
-     );
- });
+// Compress CSS
+gulp.task('compress-css', function(){
+    return gulp.src(paths.styles.css + '*.css')
+        .pipe($.cleancss({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest(paths.styles.css))
+        .pipe($.notify({ message: 'CSS compression task complete' }));
+});
 
- // Watch
- gulp.task('watch', function() {
-     // Watch Scss
- 	gulp.watch(paths.scss + '**/*.scss', ['scss']);
- });
+// Watch
+gulp.task('watch', ['scss', 'exec-postcss'], function() {
+    gulp.watch(paths.styles.scss + '**/*.scss', ['scss']); // watch the same files in our sass task
+});
 
+// Default task
+gulp.task('default', ['watch']);
 
- gulp.task('default', ['dev']);
-
- gulp.task('dev', ['scss', 'watch']);
-
- gulp.task('build', ['scss']);
+// Build task
+gulp.task('build', ['scss', 'exec-postcss', 'compress-css']);
