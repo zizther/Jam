ef8dcefeedaeec00c1974f340c710559bfe76185
Nathan Reed <zizther@gmail.com>
2018-06-15 10:22:42 +0100

    Gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -2,62 +2,59 @@
  * Variables
  */
 var paths = {
-    styles: {
-        scss: './scss/',
-        css: './css/'
-    }
-};
+        styles: {
+            scss: './scss/',
+            css: './css/'
+        }
+    },
+    autoprefixerBrowsers = ['last 4 versions', 'ie >= 10'],
+    sassPrecision = 10;
 
 
 /*
  * NPM Packages
  */
 var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins'),
-    exec = require('child_process').exec,
-    pump = require('pump'),
-	autoprefixer = require('autoprefixer');
-
-const $ = gulpLoadPlugins(); // Load all plugins which start with 'gulp-'. Reference using $.
+	notify = require('gulp-notify'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	sass = require('gulp-sass'),
+	gcmq = require('gulp-group-css-media-queries'),
+    cssnano = require('gulp-cssnano'),
+    pump = require('pump');
 
 
 /*
  * Tasks
  */
-// NPM Tasks
-gulp.task('exec-postcss', function (cb) {
-    exec('npm run postcss', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-})
-
 // Scss
-gulp.task('scss', function() {
-    gulp.src(paths.styles.scss + '**/*.scss')
-    	.pipe($.sass({
-	    	precision: 10,
-	    	sourceComments: false,
-	    	outputStyle: 'compressed'
-    	}).on('error', $.sass.logError))
-		.pipe(gulp.dest(paths.styles.css))
-        .pipe($.notify({ message: 'Scss task complete' }));
-});
+gulp.task('scss', function(cb){
+    var processors = [
+            autoprefixer({
+                browsers: autoprefixerBrowsers
+            })
+        ];
 
-// Compress CSS
-gulp.task('compress-css', function(){
-    return gulp.src(paths.styles.css + '*.css')
-        .pipe($.cleancss({debug: true}, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(gulp.dest(paths.styles.css))
-        .pipe($.notify({ message: 'CSS compression task complete' }));
+    pump(
+        [
+            gulp.src(paths.styles.scss + '**/*.scss'),
+	        sass({
+    	        precision: sassPrecision,
+    	        sourceComments: false,
+    	        outputStyle: 'compressed'
+	        }).on('error', sass.logError),
+        	postcss(processors),
+            gcmq(),
+            cssnano(),
+    		gulp.dest(paths.styles.css),
+            notify({ message: 'Scss task complete' })
+        ],
+        cb
+    );
 });
 
 // Watch
-gulp.task('watch', ['scss', 'exec-postcss'], function() {
+gulp.task('watch', ['scss'], function() {
     gulp.watch(paths.styles.scss + '**/*.scss', ['scss']); // watch the same files in our sass task
 });
 
@@ -65,4 +62,4 @@ gulp.task('watch', ['scss', 'exec-postcss'], function() {
 gulp.task('default', ['watch']);
 
 // Build task
-gulp.task('build', ['scss', 'exec-postcss', 'compress-css']);
+gulp.task('build', ['scss']);
